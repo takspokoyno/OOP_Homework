06b4804e08e2f7422644db2a0fe46d87a0d2f017
takspokoyno <vladburlaka2003@gmail.com>
2021-10-12 02:25:29 +0300

    OOP_Homework

---
+++ Classes1/Program.cs
@@ -0,0 +1,155 @@
+﻿
+using System;
+using System.Collections.Generic; //список
+namespace Classes
+{
+    abstract class Student //абстрактний клас
+    {
+        public Student(string name)
+        { //ім'я та порожній стейт
+            _name = name;
+            _state = "";
+        }
+        public void Relax()
+        {
+            _state += "Relax ";
+        }
+        public void Read()
+        {
+            _state += "Read ";
+        }
+        public void Write()
+        {
+            _state += "Write ";
+        }
+        public abstract void Study(); //абстрактний метод Study
+        public string _name, _state;
+    }
+
+
+    class GoodStudent : Student //успадковується від абстрактного класу
+    {
+        public GoodStudent(string name) //викликає базовий конструктор класу Студент
+            : base(name)
+        {
+            _state += "Good ";
+        }
+        override public void Study()
+        {
+            Read();
+            Write();
+            Read();
+            Write();
+            Relax();
+        }
+    }
+    class BadStudent : Student
+    {
+        public BadStudent(string name)
+            : base(name)
+        {
+            _state += "Bad ";
+        }
+        override public void Study()
+        {
+            Relax();
+            Relax();
+            Relax();
+            Relax();
+            Read();
+        }
+    }
+
+    class Group
+    {
+        private string gname;
+        List<Student> list = new List<Student>(); //список студентів 
+        public Group(string name)
+        {
+            gname = name;
+        }
+        public void AddStudent(Student stdnt)
+        { // додає студента в групу
+            list.Add(stdnt);
+        }
+        public void AddGroup(Group grp)
+        { //метод, що додає групу до групи
+            foreach (Student stdnt in grp.list)
+            {
+                AddStudent(stdnt);
+            }
+            grp.ClearGroup();
+        }
+
+        private string GetInfo()
+        { //створення рядку з інфою
+            string info = "";
+            info += gname + ": ";
+            foreach (var student in list)
+            {
+                info += student._name + " ";
+            }
+            return info;
+        }
+
+        public void PrintInfo()
+        { //виводить інформацію
+            Console.WriteLine(this.GetInfo());
+        }
+
+        private string GetFullInfo()
+        { //cтворення рядку з повною інфою
+            string info = "";
+            info += gname + ": ";
+            if (list.Count == 0)
+            {
+                info = "Група порожня";
+                return info;
+            }
+            foreach (var student in list)
+            {
+                info += student._name + ": " + student._state;
+            }
+            return info;
+        }
+
+        public void PrintFullInfo()
+        { //виводить повну інформацію
+            Console.WriteLine(this.GetFullInfo());
+        }
+        public void ClearGroup()
+        {
+            list.Clear();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   //checking
+            GoodStudent St1 = new GoodStudent("Ivanov");
+            BadStudent St2 = new BadStudent("Burlaka");
+            GoodStudent St3 = new GoodStudent("Bandyrin");
+            BadStudent St4 = new BadStudent("Polonko");
+            St1.Study();
+            St2.Study();
+            St3.Study();
+            St4.Study();
+            Group K17 = new Group("K17"); //створюємо групи
+            Group K27 = new Group("K27");
+            K17.AddStudent(St1); //Додаємо студентів 
+            K17.AddStudent(St3);
+            K27.AddStudent(St2);
+            K27.AddStudent(St4);
+            Console.WriteLine("Iнформацiя по К17:");
+            K17.PrintInfo();
+            Console.WriteLine("Iнформацiя по К27:");
+            K27.PrintInfo();
+            K17.AddGroup(K27); //додаємо групу К27 до групи К17
+            Console.WriteLine("Повна iнформацiя про групу К17:");
+            K17.PrintFullInfo();
+            Console.WriteLine("Повна iнформацiя про групу К27:");
+            K27.PrintFullInfo();
+        }
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace converter
+{
+    class Converter
+    {
+        public Converter(double eur, double usd)
+        {
+            _eur = eur;
+            _usd = usd;
+        }
+        public double HrnToEur(double hrn)
+        {
+            if (_eur != 0)
+            {
+                return Math.Round( hrn / _eur, 2);
+            }
+            else
+            {
+                Console.WriteLine("*ERROR* Divide by zero");
+                System.Environment.Exit(0);
+                return 0;
+            }
+        }
+        public double HrnToUsd(double hrn)
+        {
+            if (_usd != 0)
+            {
+                return Math.Round(hrn/_usd, 2);
+            }
+            else
+            {
+                Console.WriteLine("*ERROR* Divide by zero");
+                System.Environment.Exit(0);
+                return 0;
+            }
+        }
+        public double EurToHrn(double eur)
+        {
+                return eur * _eur;
+        }
+        public double UsdToHrn(double usd)
+        {
+                return usd * _usd;
+        }
+
+        private double _eur = 0.0;
+        private double _usd = 0.0;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            string first;
+            string second;
+            double amount;
+            Console.WriteLine("Enter current exchange rate:\n");
+            Console.WriteLine("USD: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("EUR: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Converter con = new Converter(b, a);
+            Console.WriteLine("What currency do you have? (Type h for HRN, u for USD, e for EUR)\n");
+            first = Console.ReadLine();
+            Console.WriteLine("What amount do you have? (Type double)\n");
+            amount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("what currency you want to convert to? (Type h for HRN, u for USD, e for EUR)\n");
+            second = Console.ReadLine();
+            Converter Con = new Converter(a, b);
+            if(first==second) Console.WriteLine($"Converted amount: {amount}");
+            switch (first)
+            {
+                case "h":
+                    switch (second)
+                    {
+                        case "e":
+                            Console.WriteLine($"Converted amount: {con.HrnToEur(amount)}");
+                            break;
+                        case "u":
+                            Console.WriteLine($"Converted amount: {con.HrnToUsd(amount)}");
+                            break;
+                    }
+                break;
+                case "u":
+                    switch (second)
+                    {
+                        case "e":
+                            Console.WriteLine($"Converted amount: {con.HrnToEur(con.UsdToHrn(amount))}");
+                            break;
+                        case "h":
+                            Console.WriteLine($"Converted amount: {con.UsdToHrn(amount)}");
+                            break;
+                    }
+                break;
+                case "e":
+                    switch (second)
+                    {
+                        case "h":
+                            Console.WriteLine($"Converted amount: {con.EurToHrn(amount)}");
+                            break;
+                        case "u":
+                            Console.WriteLine($"Converted amount: {con.HrnToUsd(con.EurToHrn(amount))}");
+                            break;
+                    }
+                break;
+            }
+        }
+    }
+}
